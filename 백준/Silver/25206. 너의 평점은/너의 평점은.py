7820498a5846df185a11ad9190445a660b60fcfc
HeeJin <114398091+jjin70@users.noreply.github.com>
2024-06-11 15:46:26 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,28 +1,35 @@
 sum = 0
-credit = 0 
-
+cnt = 0
 for i in range(20):
-    a, b, c = map(str, input().split())
-    if c != 'P':
-        credit += float(b)
-        if c == 'A+':
-            score = 4.5
-        elif c == 'A0':
-            score = 4.0
-        elif c == 'B+':
-            score = 3.5
-        elif c == 'B0':
-            score = 3.0
-        elif c == 'C+':
-            score = 2.5
-        elif c == 'C0':
-            score = 2.0
-        elif c == 'D+':
-            score = 1.5
-        elif c == 'D0':
-            score = 1.0
-        elif c == 'F':
-            score = 0
-        sum += float(b) * score
+  a,b,c = input().split()
+  if c == 'A+':
+    sum += float(b) * 4.5
+    cnt += float(b)
+  elif c == 'A0':
+    sum += float(b) * 4.0
+    cnt += float(b)
+  elif c == 'B+':
+    sum += float(b) * 3.5
+    cnt += float(b)
+  elif c == 'B0':
+    sum += float(b) * 3.0
+    cnt += float(b)
+  elif c == 'C+':
+    sum += float(b) * 2.5
+    cnt += float(b)
+  elif c == 'C0':
+    sum += float(b) * 2.0
+    cnt += float(b)
+  elif c == 'D+':
+    sum += float(b) * 1.5
+    cnt += float(b)
+  elif c == 'D0':
+    sum += float(b) * 1.0
+    cnt += float(b)
+  elif c == 'F':
+    sum += float(b) * 0.0
+    cnt += float(b)
+  else:
+    pass  
 
-print(sum/credit)
+print(sum/cnt)
